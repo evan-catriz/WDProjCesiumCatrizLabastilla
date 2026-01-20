2c054bc5727e895ae4dfdf637ee0d7500c24bdaf
evan-catriz <b2029eacatriz@pshs.edu.ph>
2026-01-20 10:47:15 +0800

    21st commit

--- web-pages/account/account.js
+++
@@ -1,9 +0,0 @@
-function confirmSubmit(){
-        if(!confirm("Do you want to submit?")){
-          return false;
-        }
-      }
-
-function confirmReset() {
-      return confirm("All data will be cleared. Continue?");
-    }
